803b1d59b24583954362a8718c424887a9379738
Borg <alex.kotyukov@yandex.ru>
2018-08-03 00:59:43 +0300

    Second lesson

---
+++ app/src/main/java/ru/geekbrains/lifecycle/LifeCycleActivity.java
@@ -0,0 +1,103 @@
+package ru.geekbrains.lifecycle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LifeCycleActivity extends AppCompatActivity {
+
+    private String lifeCycle = "LifeCycle: ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_cycle);
+
+        String instanceState;
+        if (savedInstanceState == null)
+            instanceState = "Первый запуск!";
+        else
+            instanceState = "Повторный запуск!";
+
+        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()", Toast.LENGTH_SHORT).show();
+
+        final TextView textCounter = findViewById(R.id.textCounter);     // Текст
+
+        // Получить презентер
+        final LifeCyclePresenter presenter = LifeCyclePresenter.getInstance();
+        // Выводим счетчик в поле
+        textCounter.setText(((Integer) presenter.getCounter()).toString());
+
+        Button button = findViewById(R.id.button);         // Кнопка
+        button.setOnClickListener(new View.OnClickListener() {      // Обработка нажатий
+            @Override
+            public void onClick(View v) {
+                presenter.incrementCounter();   // Увеличиваем счетчик на единицу
+                // Выводим счетчик в поле
+                textCounter.setText(((Integer) presenter.getCounter()).toString());
+            }
+        });
+
+        Log.v(lifeCycle, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        Log.v(lifeCycle, "onStart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState){
+        super.onRestoreInstanceState(saveInstanceState);
+        Toast.makeText(getApplicationContext(), "Повторный запуск!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.v(lifeCycle, "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+        Log.v(lifeCycle, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+        Log.v(lifeCycle, "onPause");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState){
+        super.onSaveInstanceState(saveInstanceState);
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.v(lifeCycle, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+        Log.v(lifeCycle, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+        Log.v(lifeCycle, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.v(lifeCycle, "onDestroy");
+    }
+}
